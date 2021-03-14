e3ba88d13c1d4d88353f32089dc885f1809c04cb
szocei <szocei.zoltan@gmail.com>
2021-03-14 13:34:56 +0100

    edit player

--- src/app/page/edit-player/edit-player.component.ts
+++ src/app/page/edit-player/edit-player.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Player } from 'src/app/model/player';
+import { PlayerService } from 'src/app/service/player.service';
+
 
 @Component({
   selector: 'app-edit-player',
@@ -7,7 +14,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPlayerComponent implements OnInit {
 
-  constructor() { }
+   player$: Observable<Player> = this.activatedRoute.params.pipe(
+    switchMap( params => this.playerService.get(params.id) )
+  );
+
+  constructor(
+      private activatedRoute: ActivatedRoute,
+    private playerService: PlayerService,
+    private router: Router,
+  ) { }
+
+  updating:boolean=false;
+
+  onUpdate(form: NgForm, player: Player): void {
+    if(player.id===0){
+      this.playerService.create(player)
+
+    }else{
+      this.updating=true,
+      this.playerService.update(player)
+    }
+    this.router.navigate(['player'])
+    this.playerService.getAll()
+    this.playerService.getAllsum()
+
+    
+  }
 
   ngOnInit(): void {
   }
